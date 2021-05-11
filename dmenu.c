9ce4205f1c9e701eb2e63e4a8653158b92ec1c23
Madhu <enometh@net.meer>
2021-05-11 18:02:14 +0530

    dmenu.c: (keypress): ignore XK_KP keypad events

--- dmenu.c
+++ dmenu.c
@@ -340,11 +340,9 @@ keypress(XKeyEvent *ev)
 			                  utf8, utf8, win, CurrentTime);
 			return;
 		case XK_Left:
-		case XK_KP_Left:
 			movewordedge(-1);
 			goto draw;
 		case XK_Right:
-		case XK_KP_Right:
 			movewordedge(+1);
 			goto draw;
 		case XK_Return:
@@ -382,7 +380,6 @@ insert:
 			insert(buf, len);
 		break;
 	case XK_Delete:
-	case XK_KP_Delete:
 		if (text[cursor] == '\0')
 			return;
 		cursor = nextrune(+1);
@@ -393,7 +390,6 @@ insert:
 		insert(NULL, nextrune(-1) - cursor);
 		break;
 	case XK_End:
-	case XK_KP_End:
 		if (text[cursor] != '\0') {
 			cursor = strlen(text);
 			break;
@@ -413,7 +409,6 @@ insert:
 		cleanup();
 		exit(1);
 	case XK_Home:
-	case XK_KP_Home:
 		if (sel == matches) {
 			cursor = 0;
 			break;
@@ -422,7 +417,6 @@ insert:
 		calcoffsets();
 		break;
 	case XK_Left:
-	case XK_KP_Left:
 		if (cursor > 0 && (!sel || !sel->left || lines > 0)) {
 			cursor = nextrune(-1);
 			break;
@@ -431,7 +425,6 @@ insert:
 			return;
 		/* fallthrough */
 	case XK_Up:
-	case XK_KP_Up:
 		if (sel && sel->left) {
 			sel = sel->left;
 			if (sel->right == curr) {
@@ -445,7 +438,6 @@ insert:
 		}
 		break;
 	case XK_Next:
-	case XK_KP_Next:
 		if (!next) {
 			//;madhu 171111 return;
 			fprintf(stderr, "madhu: next\n");
@@ -457,7 +449,6 @@ insert:
 		calcoffsets();
 		break;
 	case XK_Prior:
-	case XK_KP_Prior:
 		if (!prev) {
 			// ;madhu 171111 return;
 			fprintf(stderr, "madhu: prior\n");
@@ -479,7 +470,6 @@ insert:
 			sel->out = 1;
 		break;
 	case XK_Right:
-	case XK_KP_Right:
 		if (text[cursor] != '\0') {
 			cursor = nextrune(+1);
 			break;
@@ -488,7 +478,6 @@ insert:
 			return;
 		/* fallthrough */
 	case XK_Down:
-	case XK_KP_Down:
 		if (sel && sel->right) {
 			sel = sel->right;
 			if (sel == next) {
